414bc3ecfbd45b99cfbecd6c4292b2c3b974adfc
heline-m <eline.malherbe@epsi.fr>
2021-11-28 14:20:59 +0100

    DAL et depot de Adherents et ListeAchat finis
    les tests sont faits mais il reste quelques erreurs donc je ne les commit pas avec

---
+++ EMI_RA.DAL/Adherents_DAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace EMI_RA.DAL
+{
+    public class Adherents_DAL
+    {
+        public int ID { get; set; }
+
+        public List<Adherents_DAL> Adherents { get; set; }
+        public String Societe { get; set; }
+        public String CiviliteContact { get; set; }
+        public String NomContact { get; set; }
+        public String PrenomContact { get; set; }
+        public String Email { get; set; }
+        public String Adresse { get; set; }
+        public DateTime? DateAdhesion { get; set; }
+
+        //constructeur par défaut 
+        public Adherents_DAL(String societe, String civiliteContact, String nomContact, String prenomContact, String email, String adresse)
+        =>(Societe, CiviliteContact, NomContact, PrenomContact, Email, Adresse) = (societe, civiliteContact, nomContact, prenomContact, email, adresse);
+        public Adherents_DAL(int idAdherents, String societe, String civiliteContact, String nomContact, String prenomContact, String email, String adresse, DateTime? dateAdhesion)
+                    => (ID, Societe, CiviliteContact, NomContact, PrenomContact, Email, Adresse, DateAdhesion) = (idAdherents, societe, civiliteContact, nomContact, prenomContact, email, adresse, dateAdhesion);
+
+
+        // TODO : remplacer le string ad par le Fournisseur une fois cet objet créé
+        public void Insert()
+        {
+            var chaineConnexion = @"Data Source=localhost\sql2019;Initial Catalog=EMI-r;Integrated Security=True";
+
+            //Créer une connexion
+            using (var connexion = new SqlConnection(chaineConnexion))
+            {
+                //ouvrir la connexion
+                connexion.Open();
+
+                //créer une commande pour l'instruction SQL à executer
+                using (var commande = new SqlCommand())
+                {
+                    //définir la connexion à utiliser
+                    commande.Connection = connexion;
+
+                    //définir l'instruction SQL
+                    //avec des paramètres si besoin
+                    //SELECT SCOPE_IDENTITY() va renvoyer l'ID créé
+                    commande.CommandText = "insert into adherent (societe, civiliteContact, nomContact, prenomContact, email, adresse, dateAdhesion)"
+                                            + " values (@societe, @civiliteContact, @nomContact, @prenomContact, @email, @adresse, GetDate()); select scope_identity()";
+                    commande.Parameters.Add(new SqlParameter("@societe", Societe));
+                    commande.Parameters.Add(new SqlParameter("@civiliteContact", CiviliteContact));
+                    commande.Parameters.Add(new SqlParameter("@nomContact", NomContact));
+                    commande.Parameters.Add(new SqlParameter("@prenomContact", PrenomContact));
+                    commande.Parameters.Add(new SqlParameter("@email", Email));
+                    commande.Parameters.Add(new SqlParameter("@adresse", Adresse));
+                    ID = (int)commande.ExecuteScalar();
+                }
+
+                //fermer la connexion
+                connexion.Close();
+            }
+        }
+    }
+}
---
+++ EMI_RA.DAL/Adherents_Depot_DAL.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMI_RA.DAL
+{
+    public class Adherents_Depot_DAL : Depot_DAL<Adherents_DAL>
+    {
+        public Adherents_Depot_DAL()
+            :base()
+        {
+
+        }
+
+        public override List<Adherents_DAL> GetAll()
+        {
+            CreerConnexionEtCommande();
+
+            commande.CommandText = "select idAdherents, societe, civiliteContact, nomContact, prenomContact, email, adresse, dateAdhesion from adherents";
+            //pour lire les lignes une par une
+            var reader = commande.ExecuteReader();
+
+            var listeDeAdherents = new List<Adherents_DAL>();
+
+            while (reader.Read())
+            {
+                //dans reader.GetInt32 on met la colonne que l'on souhaite récupérer ici 0 = ID, 1 = Societe...
+                var adherent = new Adherents_DAL(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetDateTime(7));
+
+                listeDeAdherents.Add(adherent);
+            }
+
+            DetruireConnexionEtCommande();
+
+            return listeDeAdherents;
+        }
+
+        public override Adherents_DAL GetByID(int ID)
+        {
+            CreerConnexionEtCommande();
+
+            commande.CommandText = "select idAdherents, societe, civiliteContact, nomContact, prenomContact, email, adresse, dateAdhesion from adherents"
+            +" where idAdherents=@ID";
+            commande.Parameters.Add(new SqlParameter("@ID", ID));
+            var reader = commande.ExecuteReader();
+
+            var listeDeAdherents = new List<Adherents_DAL>();
+
+            Adherents_DAL adherent;
+            if (reader.Read())
+            {
+                adherent = new Adherents_DAL(reader.GetInt32(0),
+                                        reader.GetString(1),
+                                        reader.GetString(2),
+                                        reader.GetString(3),
+                                        reader.GetString(4),
+                                        reader.GetString(5),
+                                        reader.GetString(6),
+                                        reader.GetDateTime(7));
+            }
+            else
+                throw new Exception($"Pas de adherent dans la BDD avec l'ID {ID}");
+
+            DetruireConnexionEtCommande();
+
+            return adherent;
+        }
+
+        public override Adherents_DAL Insert(Adherents_DAL adherent)
+        {
+            CreerConnexionEtCommande();
+
+            commande.CommandText = "insert into adherents (societe, civiliteContact, nomContact, prenomContact, email, adresse, dateAdhesion)"
+                                    + " values (@societe, @civiliteContact, @nomContact, @prenomContact, @email, @adresse, GetDate()); select scope_identity()";
+            commande.Parameters.Add(new SqlParameter("@societe", adherent.Societe));
+            commande.Parameters.Add(new SqlParameter("@civiliteContact", adherent.CiviliteContact));
+            commande.Parameters.Add(new SqlParameter("@nomContact", adherent.NomContact));
+            commande.Parameters.Add(new SqlParameter("@prenomContact", adherent.PrenomContact));
+            commande.Parameters.Add(new SqlParameter("@email", adherent.Email));
+            commande.Parameters.Add(new SqlParameter("@adresse", adherent.Adresse));
+
+            var ID = Convert.ToInt32((decimal)commande.ExecuteScalar());
+
+            adherent.ID = ID;
+
+            DetruireConnexionEtCommande();
+
+            return adherent;
+        }
+
+        public override Adherents_DAL Update(Adherents_DAL adherent)
+        {
+            CreerConnexionEtCommande();
+
+            commande.CommandText = "update adherents set societe = @societe, civiliteContact = @civiliteContact, nomContact = @nomContact, prenomContact = @prenomContact, email = @email, adresse = @adresse "
+                                    + " where idAdherents=@ID";
+            commande.Parameters.Add(new SqlParameter("@ID", adherent.ID));
+            commande.Parameters.Add(new SqlParameter("@societe", adherent.Societe));
+            commande.Parameters.Add(new SqlParameter("@civiliteContact", adherent.CiviliteContact));
+            commande.Parameters.Add(new SqlParameter("@nomContact", adherent.NomContact));
+            commande.Parameters.Add(new SqlParameter("@prenomContact", adherent.PrenomContact));
+            commande.Parameters.Add(new SqlParameter("@email", adherent.Email));
+            commande.Parameters.Add(new SqlParameter("@adresse", adherent.Adresse));
+            var nombreDeLignesAffectees = (int)commande.ExecuteNonQuery();
+
+            if (nombreDeLignesAffectees != 1)
+            {
+                throw new Exception($"Impossible de mettre à jour l'adhérent avec l'ID  {adherent.ID}");
+            }
+
+            DetruireConnexionEtCommande();
+
+            return adherent;
+        }
+
+        public override void Delete(Adherents_DAL adherent)
+        {
+            CreerConnexionEtCommande();
+
+            commande.CommandText = "delete from adherents where idAdherents = @ID";
+            commande.Parameters.Add(new SqlParameter("@ID", adherent.ID));
+            var nombreDeLignesAffectees = (int)commande.ExecuteNonQuery();
+
+            if (nombreDeLignesAffectees != 1)
+            {
+                throw new Exception($"Impossible de supprimer l'adherent avec l'ID {adherent.ID}");
+            }
+
+            DetruireConnexionEtCommande();
+        }
+    }
+}
