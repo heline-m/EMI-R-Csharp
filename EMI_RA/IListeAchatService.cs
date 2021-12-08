7028d4dac2f3513cd9526e965b97bb45e0201075
heline-m <eline.malherbe@epsi.fr>
2021-12-08 21:11:40 +0100

    Ajout Lignes, ILIgnesService, LignesService, ListeAchat, ListeAchatService, IListeAchatService

---
+++ EMI_RA/IListeAchatService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMI_RA
+{
+    public interface IListeAchatService
+    {
+        public List<ListeAchat> GetAllListeAchat();
+        public ListeAchat GetListeAchatByID(int IdListeAchat);
+        public ListeAchat Insert(ListeAchat l);
+        public ListeAchat Update(ListeAchat l);
+        public void Delete(ListeAchat l);
+
+    }
+}
---
+++ EMI_RA/ListeAchat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMI_RA
+{
+    public class ListeAchat
+    {
+        
+        public int IdListesDAchats { get; set; }
+        public int IdAdherents { get; set; }
+        public int IdPaniersGlobaux { get; set; }
+        public int Annee { get; set; }
+        public int NumeroSemaine { get; set; }
+
+        //constructeur par défaut 
+        public ListeAchat(int idAdherents, int idPaniersGlobaux, int annee, int numeroSemaine)
+        => (IdAdherents, IdPaniersGlobaux, Annee, NumeroSemaine) = (idAdherents, idPaniersGlobaux, annee, numeroSemaine);
+        public ListeAchat(int idListesDAchats, int idAdherents, int idPaniersGlobaux, int annee, int numeroSemaine)
+                    => (IdListesDAchats, IdAdherents, IdPaniersGlobaux, Annee, NumeroSemaine) = (idListesDAchats, idAdherents, idPaniersGlobaux, annee, numeroSemaine);
+
+    }
+}
