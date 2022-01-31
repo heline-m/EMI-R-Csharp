4cafe4b4af1933ba0a3bf7f5adf6aa914db3e3ad
ines-boucard <72189183+ines-boucard@users.noreply.github.com>
2022-01-31 21:07:01 +0100

    changement commande

--- EMI_RA.WPF/Commande.xaml.cs
+++ EMI_RA.WPF/Commande.xaml.cs
@@ -30,21 +30,27 @@ namespace EMI_RA.WPF
             InitializeComponent();
         }
 
-        private async void Choisir_Click(object sender, RoutedEventArgs e)
+        private void Choisir_Click(object sender, RoutedEventArgs e)
         {
+           // MessageBox.Show("hello");
+
             OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
             OpenFileDialog1.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
             OpenFileDialog1.ShowDialog();
 
-         var  open = OpenFileDialog1.OpenFile();
-
+            var  open = OpenFileDialog1.OpenFile();
+         
 
             var nom = OpenFileDialog1.FileName;
 
             string ligne;
             int compteur = 0;
-
-          StreamReader reader = new StreamReader(open);
+       OpenFileDialog opfd = new OpenFileDialog();
+            if(opfd.ShowDialog() == true)
+            {
+                txt.Text = File.ReadAllText(opfd.FileName);
+            }
+                StreamReader reader = new StreamReader(open);
             while ((ligne = reader.ReadLine()) != null){
                 String[] substring = ligne.Split(' ');
                 foreach (String s in substring)
@@ -53,8 +59,10 @@ namespace EMI_RA.WPF
                 }
                 compteur++;
             }
+            FileParameter file = new FileParameter(open);
             var clientApi = new Client("https://localhost:44313/", new HttpClient());
-          //  var adherent = await clientApi.;
+           // var adherent =  clientApi.CommandeAsync(1,file);
+            clientApi.CommandeAsync(1, file);
 
             /*   using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx"})
                {
@@ -63,6 +71,28 @@ namespace EMI_RA.WPF
 
                    }
                }*/
+
+
+        }
+        private void Choisir2_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string fileText = "reference ;quantite ;prix unitaire HT";
+
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Panier";
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "Text documents (.csv)|*.csv";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Save document
+                // string filename = dlg.FileName;
+                File.WriteAllText(dlg.FileName, fileText);
+            }
+
         }
     }
 }
